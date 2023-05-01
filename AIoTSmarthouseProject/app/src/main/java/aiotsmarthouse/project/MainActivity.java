5882e97546342323b22d28b39b1b90994a912720
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2023-05-01 21:16:08 +0700

    Git fixing the UI

--- AIoTSmarthouseProject/app/src/main/java/aiotsmarthouse/project/MainActivity.java
+++ AIoTSmarthouseProject/app/src/main/java/aiotsmarthouse/project/MainActivity.java
@@ -3,6 +3,8 @@ package aiotsmarthouse.project;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.github.angads25.toggle.interfaces.OnToggledListener;
@@ -23,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Begin full screen app
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //end full screen app
+
         setContentView(R.layout.activity_main);
 
         txtTemp = findViewById(R.id.txtTemp);
